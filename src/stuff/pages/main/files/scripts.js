9565ff4153f22ebd3f214ef8661338301d84aba5
o7q <jev.o7q@gmail.com>
2023-02-27 22:52:03 -0600

    Update scripts

--- src/stuff/pages/main/files/scripts.js
+++ src/stuff/pages/main/files/scripts.js
@@ -1,5 +1,36 @@
+function pageInit()
+{
+    constrainMobile();
+    welcomeText();
+}
+
 // constrain mobile
 function constrainMobile()
 {
     if (mobileCheck() == true) openPage("replace", "../stuff/index.html");
+}
+
+function welcomeText()
+{
+    const message =
+    [
+        "Welcome to ",
+        // "You are viewing ",
+        // "You are on "
+    ];
+    const messageWidth =
+    [
+        152,
+        // 206,
+        // 136
+    ];
+
+    let randMsg = Math.floor(Math.random() * message.length);
+
+    changeWelcomeText(message[randMsg], messageWidth[randMsg])
+}
+function changeWelcomeText(message, messageWidth)
+{
+    document.getElementById("jsRandomText").innerHTML = message + " ";
+    document.querySelector(".arrowImg").style.transform = "translateX(" + messageWidth + "px)";
 }
