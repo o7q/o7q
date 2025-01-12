e59432f0637c71e51422c6026edb53a095ce49ab
James <103224608+o7q@users.noreply.github.com>
2025-01-12 14:38:30 -0600

    Update sly gif

--- files/renders/seq2gif_png_256x256_12.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Generating GIF...
-
-ffmpeg -y -i "%%04d.png" -vf "scale=512:256,palettegen=reserve_transparent=1:max_colors=32" "palette.png"
-ffmpeg -framerate 24 -y -i "%%04d.png" -i "palette.png" -lavfi "scale=512:256,paletteuse=alpha_threshold=128" -gifflags -offsetting "sly.gif"
-
-title Done!
-rem pause
